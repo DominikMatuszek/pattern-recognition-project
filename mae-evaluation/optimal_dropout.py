095a20462938588d388c924da8f51c00aa2a0179
ignacy.alwasiak@gmail.com <ignacy.alwasiak@gmail.com>
2024-01-20 19:34:37 +0100

    added model eval as input

--- mae-evaluation/optimal_dropout.py
+++ mae-evaluation/optimal_dropout.py
@@ -6,6 +6,7 @@ from optimal_sampler import ProgrammableSampler
 def main():
     validator = Validator("optimal_dropout.txt", size=224)
     device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    model = torch.load("./model_state.pth")
 
     for dropout in range(0, 10):
         dp = dropout / 10
@@ -15,6 +16,7 @@ def main():
             dropout_rate=dp,
             device=device,
             query_model_override="timm/eva02_tiny_patch14_224.mim_in22k",
+            saliency_fn=model
         )
 
         validator.log_accuracy_for_model(model, device, f"VIT with optimal sampling dropout {dp}")
